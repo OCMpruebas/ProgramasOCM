7b19ad8b84acfb781f0aacadaa8076fb463b459e
mamjerez <correomam@gmail.com>
2018-09-30 12:06:29 +0200

    Cambios menores domingo por la mañana.

--- src/app/programas/programa/programa-anios/programa-anios.component.ts
+++ src/app/programas/programa/programa-anios/programa-anios.component.ts
@@ -1,25 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-programa-anios',
   templateUrl: './programa-anios.component.html',
   styleUrls: ['./programa-anios.component.css']
 })
-
-
 export class ProgramaAniosComponent implements OnInit {
-DesPro = 'Recogida de residuos';
+  DesPro = 'Recogida de residuos';
 
   constructor() {
-
     function clearBox(elementID) {
-    document.getElementById(elementID).innerHTML = '';
-}
-       }
+      document.getElementById(elementID).innerHTML = '';
+    }
+  }
 
   ngOnInit() {}
-    }
-
-
-
+}
