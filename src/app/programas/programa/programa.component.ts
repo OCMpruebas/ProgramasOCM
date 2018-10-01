4173bd18b5d2125beda20cc348097cd92daf88eb
mamjerez <correomam@gmail.com>
2018-10-01 11:19:49 +0200

    Funciona Choosen, problema con jquery.

--- src/app/programas/programa/programa.component.ts
+++ src/app/programas/programa/programa.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+jQuery(document).ready(function() {
+  console.log('Pagina cargada');
+  $('h1').text('Hola desde jquery');
+  jQuery.getScript('//kkharvesthq.github.io/chosen/chosen.jquery.js')
+    .done(function(script, textStatus) {
+      $('.chosen-select').chosen({no_results_text: 'Oops, no se ha encontrado nada!'});
+    })
+    .fail(function(jqxhr, settings, exception) {
+      alert('Error en programa.component.ts');
+    });
+});
+
+/* jQuery(document).ready(function(){
+    jQuery.getScript( "//harvesthq.github.io/chosen/chosen.jquery.js" )
+      .done(function( script, textStatus ) {
+          jQuery(".chosen1").chosen();
+          jQuery(".chosen2").chosen({max_selected_options: 2});
+          jQuery(".chosen2").bind("chosen:maxselected", function () {alert("Máximo número de elementos seleccionado")});
+          jQuery(".chosen3").chosen({allow_single_deselect: true, disable_search_threshold: 100});
+      })
+      .fail(function( jqxhr, settings, exception ) {
+           alert("Error");
+  });
+
+}); */
+
 
 @Component({
   selector: 'app-programa',
   templateUrl: './programa.component.html',
   styleUrls: ['./programa.component.css']
 })
+
+// $('.chosen-select').chosen({no_results_text: 'Oops, nothing found!'});
 export class ProgramaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
