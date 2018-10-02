a7400184544254fe9e41b6bb0e26ce6cf91bbe7d
mamjerez <correomam@gmail.com>
2018-10-02 23:12:31 +0200

    Usando programas.json

--- src/app/programas/programa/programa.component.ts
+++ src/app/programas/programa/programa.component.ts
@@ -13,32 +13,17 @@ $(document).ready(function() {
     });
 });
 
-/* jQuery(document).ready(function(){
-    jQuery.getScript( "//harvesthq.github.io/chosen/chosen.jquery.js" )
-      .done(function( script, textStatus ) {
-          jQuery(".chosen1").chosen();
-          jQuery(".chosen2").chosen({max_selected_options: 2});
-          jQuery(".chosen2").bind("chosen:maxselected", function () {alert("Máximo número de elementos seleccionado")});
-          jQuery(".chosen3").chosen({allow_single_deselect: true, disable_search_threshold: 100});
-      })
-      .fail(function( jqxhr, settings, exception ) {
-           alert("Error");
-  });
-
-}); */
-
-$(function () {
-    let countryOptions;
-   $.getJSON('/assets/data/countries.json', function (result) {
-   $.each(result, function (i, country) {
-  // <option value='countrycode'>contryname</option>
-  // countryOptions += '<option value =' + country.code + '>' + country.name + '</option>';
-  countryOptions += '<option value =' + '>' + country.name + '</option>';
-   $('#country').html(countryOptions);
-    });
-    });
-    });
-
+    $(function () {
+      let countryOptions;
+     $.getJSON('/assets/data/programas.json', function (result) {
+     $.each(result, function (i, programa) {
+    // <option value='countrycode'>contryname</option>
+    // countryOptions += '<option value =' + country.code + '>' + country.name + '</option>';
+    countryOptions += '<option value =' + '>' + programa.DesPro + '</option>';
+     $('#country').html(countryOptions);
+      });
+      });
+      });
 
 @Component({
   selector: 'app-programa',
@@ -46,7 +31,6 @@ $(function () {
   styleUrls: ['./programa.component.css']
 })
 
-// $('.chosen-select').chosen({no_results_text: 'Oops, nothing found!'});
 export class ProgramaComponent implements OnInit {
   constructor() {}
 
